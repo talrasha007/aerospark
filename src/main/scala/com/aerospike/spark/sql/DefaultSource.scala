eacfa3c31fb671d70a288014efe9e5a7d8ed9dba
ttl <talrasha007@gmail.com>
2016-08-09 16:45:42 +0800

    user UPDATE mode for append.

--- src/main/scala/com/aerospike/spark/sql/DefaultSource.scala
+++ src/main/scala/com/aerospike/spark/sql/DefaultSource.scala
@@ -90,7 +90,7 @@ with CreatableRelationProvider{
 			case SaveMode.ErrorIfExists => policy.recordExistsAction = RecordExistsAction.CREATE_ONLY
 			case SaveMode.Ignore => policy.recordExistsAction = RecordExistsAction.CREATE_ONLY
 			case SaveMode.Overwrite => policy.recordExistsAction = RecordExistsAction.REPLACE
-			case SaveMode.Append => policy.recordExistsAction = RecordExistsAction.UPDATE_ONLY
+			case SaveMode.Append => policy.recordExistsAction = RecordExistsAction.UPDATE
 		}
 
 		val genPol = config.get(AerospikeConfig.generationPolicy)
